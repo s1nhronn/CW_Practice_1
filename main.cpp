60b6e337b83c19905ddec3687ee9e5fb28b664ab
s1nhronn <silekatik@yandex.ru>
2026-09-16 09:55:03 +0300

    Reading the argument

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <cerrno>
 #include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
 
 namespace mtt
 {
@@ -22,5 +26,24 @@ namespace mtt
   };
 }
 
-int main()
-{}
+int main(int argc, char* argv[])
+{
+  if (argc < 2)
+  {
+    std::cerr << "Argument is not specified" << '\n';
+    return 1;
+  }
+
+  char* endptr = nullptr;
+  errno = 0;
+  long val = std::strtol(argv[1], &endptr, 10);
+
+  using limit_t = std::numeric_limits< int >;
+  if (endptr == argv[1] || *endptr != '\0' || errno == ERANGE || val < limit_t::min() || val > limit_t::max())
+  {
+    std::cerr << "Incorrect argument" << '\n';
+    return 2;
+  }
+
+  // int n = static_cast< int >(val);
+}
